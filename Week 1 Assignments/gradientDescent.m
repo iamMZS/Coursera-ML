f42edee6a7fb17979feb8ed5af435b3a25eb8332
iamMZS <zash.zeeshan@gmail.com>
2017-06-21 05:44:30 +0530

    Added

--- Week 1 Assignments/gradientDescent.m
+++ Week 1 Assignments/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 
-m = length(y); % number of training examples
+m = length(y); % number of training example
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
